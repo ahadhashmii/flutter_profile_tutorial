3aa8ecae9651e1fb53b7f48c74df54e69bd6da41
Ahad <ahadhashmideveloper@gmail.com>
2023-03-13 14:49:07 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('assets/images/user.JPG'),
+            ),
+            const SizedBox(height: 20),
+            itemProfile('Name', 'Ahad Hashmi', CupertinoIcons.person),
+            const SizedBox(height: 10),
+            itemProfile('Phone', '03107085816', CupertinoIcons.phone),
+            const SizedBox(height: 10),
+            itemProfile('Address', 'abc address, xyz city', CupertinoIcons.location),
+            const SizedBox(height: 10),
+            itemProfile('Email', 'ahadhashmideveloper@gmail.com', CupertinoIcons.mail),
+            const SizedBox(height: 20,),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  child: const Text('Edit Profile')
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  itemProfile(String title, String subtitle, IconData iconData) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 5),
+                color: Colors.deepOrange.withOpacity(.2),
+                spreadRadius: 2,
+                blurRadius: 10
+            )
+          ]
+      ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+        tileColor: Colors.white,
+      ),
+    );
+  }
+}
+
